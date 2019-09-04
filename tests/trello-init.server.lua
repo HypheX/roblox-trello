3b31d8a64b5c70545ff9c5d929075fd987df13f0
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2019-09-04 16:22:10 +0100

    Add MIT copyright notice to all source files

--- tests/trello-init.server.lua
+++ tests/trello-init.server.lua
@@ -2,8 +2,20 @@
     Name: trello-init.server.lua
     From roblox-trello v2
 
-    Description: Makes some testing on the 
-    This script is in public domain.
+    Description: Performs some testing on the Trello v2 features.
+
+
+    Copyright (c) 2019 Luis, David Duque
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
 --]]
 
 local Trello = require(game.ServerScriptService.Trello)
