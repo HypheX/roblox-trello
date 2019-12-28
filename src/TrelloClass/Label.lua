6b3442a968dd5d413ba9244818af8dfec30cec41
David Duque <david.f.s.duque@tecnico.ulisboa.pt>
2019-12-28 14:49:51 +0000

    Implement Labels

--- src/TrelloClass/Label.lua
+++ src/TrelloClass/Label.lua
@@ -19,7 +19,6 @@
 --]]
 
 local HTTP = require(script.Parent.Parent.TrelloHttp)
-local LabelColor = require(script.Parent.LabelColor)
 
 local TrelloLabelMeta = {
     __tostring = "TrelloLabel",
@@ -36,3 +35,102 @@ local TrelloLabelMeta = {
         error("[TrelloLabel]: Attempted to set non-existant property "..tostring(index)..".", 0)
     end
 }
+
+-- Local function prototype to make a board based on a body dump
+local makeLabel
+
+local TrelloLabel = {}
+
+--[[**
+    Creates a new label and assigns it to the board
+
+    @param [t:String] name The label's name. Must to be a string with a maximum of 16384 characters.
+    @param [t:LabelColor] color The label's color. 
+    @param [t:TrelloClient] board The board where the label will be attached to.
+
+    @returns [t:TrelloBoard] A new TrelloBoard that was freshly created.
+**--]]
+function TrelloLabel.new(name, color, board)
+    local commitURL = board.Client:MakeURL("/labels", {
+        name = name,
+        color = color,
+        idBoard = board.RemoteId
+    })
+
+    local labelDump = HTTP.RequestInsist(commitURL, HTTP.HttpMethod.POST, "{}", true)
+    local label = makeLabel(labelDump.Body)
+
+    board._pkg.appendLabel(label)
+
+    return label
+end
+
+makeLabel = function(board, data)
+    if not data then
+        return nil
+    end
+
+    local tracking = {
+        Name = data.name,
+        Color = data.color
+    }
+
+    local trelloLabel = {
+        RemoteId = data.id,
+        Board = board,
+        Loaded = true,
+        Name = data.name,
+        Color = data.color
+    }
+
+    --[[**
+        Pushes the label's changes to Trello.
+
+        @param [t:Boolean] force Whether to push all changes to the board even though nothing has been changed.
+
+        @returns [t:Void]
+    **--]]
+    function trelloLabel:Update(force)
+        local count = 0
+        local commit = {}
+
+        for i, v in pairs (tracking) do
+            if v ~= self[i] or force then
+                commit[i] = self[i]
+                count = count + 1
+            end
+        end
+
+        if count == 0 then
+            warn("[Trello/Label.Update]: Nothing to change. Skipping.")
+        end
+
+        local commitURL = board.Client:MakeURL("/labels/"..self.RemoteId, {
+            name = commit.Name,
+            color = commit.Color
+        })
+
+        HTTP.RequestInsist(commitURL, HTTP.HttpMethod.PUT, "{}", true)
+
+        for i, v in pairs(commit) do
+            tracking[i] = v
+        end
+    end
+
+    --[[**
+        Deletes the label from Trello. All garbage collection is up to the developer to perform.
+
+        @returns [t:Void]
+    **--]]
+    function trelloLabel:Delete()
+        local commitURL = board.Client:MakeURL("/labels/" .. self.RemoteId)
+
+        HTTP.RequestInsist(commitURL, HTTP.HttpMethod.DELETE, nil, true)
+
+        trelloLabel = nil
+    end
+
+    return setmetatable(trelloLabel, TrelloLabelMeta)
+end
+
+return {Public = TrelloLabel, Make = makeLabel}
